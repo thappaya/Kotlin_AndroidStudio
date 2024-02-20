273d8b83ae1eadd09a6deded420d47566c58e157
Thappaya Wangsantiporn <thappaya.w@gmail.com>
2024-02-20 21:43:03 +0700

    More functionality added, also removing some unnecessary elements.

--- app/src/main/java/com/a4d/retrorumble/PS3Fragment.kt
+++ app/src/main/java/com/a4d/retrorumble/PS3Fragment.kt
@@ -6,28 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.a4d.retrorumble.databinding.FragmentPlaystationBinding
-
-class PlayStationFragment: Fragment() {
-    private var _binding: FragmentPlaystationBinding? = null
+import com.a4d.retrorumble.databinding.FragmentPs3Binding
+class PS3Fragment: Fragment() {
+    private var _binding: FragmentPs3Binding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPlaystationBinding.inflate(inflater, container, false)
+        _binding = FragmentPs3Binding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.backButton5.setOnClickListener {
-            findNavController().navigate(R.id.action_PlayStationFragment_to_ShopFragment)
-        }
-        binding.ps3Button.setOnClickListener {
-            findNavController().navigate(R.id.action_PlayStationFragment_to_PS3Fragment)
+
+        binding.backButton9.setOnClickListener {
+            findNavController().navigate(R.id.action_PS3Fragment_to_PlayStationFragment)
         }
     }
 
